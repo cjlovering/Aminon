9d629748b9a1a11448a6be1d659f57ed32e614fb
Charlie <cjlovering@wpi.edu>
2016-09-29 15:17:24 -0400

    some comments

--- dns_nat_arbiter.py
+++ dns_nat_arbiter.py
@@ -4,17 +4,26 @@ import subprocess
 import random
 
 # ip_address --> ttl
+honeypot_addr = "1.1.1.1"
 store = {}
 
 def initial_configure():
+    """
+    generates list of public ips and stores it in store
+    writes the pre-routing rule to send the honeypot
+    """
     # creates a dict of ip addresses
     
-    # write the pre-routing rule
-    
+    # write the pre-routing rule    
     pass
     
 
-def iptables_call():
+def iptables_call(private_addr, public_addr, ttl, interface):
+    """
+    executes an command line ip tables command
+    """
+    
+    # TODO: generalize this for multiple comnads
     p = subprocess.Popen(["iptables", "-A", "INPUT", "-p", "tcp", "-m", "tcp", "--dport", "22" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
     output , err = p.communicate()
     print output
