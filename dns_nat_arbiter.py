435b39bb13f977bb7cd87cfcaf57e445c05a5801
jograpes <jabubes@yandex.com>
2016-09-29 15:53:38 -0400

    Update dns_nat_arbiter.py

--- dns_nat_arbiter.py
+++ dns_nat_arbiter.py
@@ -4,6 +4,7 @@ import subprocess
 import random
 
 # ip_address --> ttl
+interface = "enp0s3"
 client_addr = "0.0.0.0"
 honeypot_addr = "1.1.1.1"
 webserver_addr = "2.2.2.2"
@@ -16,7 +17,7 @@ def initial_configure():
     """
     # creates a dict of ip addresses
     
-    # write the pre-routing rule
+    # write the pre-routing rule to route everything to the honeypot unless otherwise specified
     """
     iptables -A FORWARD -d 2.2.2.2 -i eth0 -p tcp -m tcp --dport 1000:65500 -j ACCEPT #forward tcp port range
     iptables -A FORWARD -d 2.2.2.2 -i eth0 -p udp -m udp --dport 1000:65500 -j ACCEPT #forward udp port range
@@ -24,19 +25,19 @@ def initial_configure():
     iptables -t nat -A PREROUTING -d 1.1.1.1 -p udp -m udp --dport 1000:65500 -j DNAT --to-destination 2.2.2.2  #udp port range
     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
     """
-    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", "enp0s3", "-p", "tcp", "-m", "tcp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", "enp0s3", "-p", "udp", "-m", "udp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", interface, "-p", "tcp", "-m", "tcp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", interface, "-p", "udp", "-m", "udp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
     sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-d", client_addr, "-p", "tcp", "-m", "tcp", "--dport", "1000:65500" , "-j", "DNAT", "--to-destination", honeypot_addr], stdout=subprocess.PIPE)
     sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-d", client_addr, "-p", "udp", "-m", "udp", "--dport", "1000:65500" , "-j", "DNAT", "--to-destination", honeypot_addr], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", "enp0s3", "-j", "MASQUERADE"], stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", interface, "-j", "MASQUERADE"], stdout=subprocess.PIPE)
     output , err = sp.communicate()
     print output
     pass
     
 
-def iptables_call(private_addr, public_addr, ttl, interface):
+def approve_ip(private_addr, public_addr, ttl, interface):
     """
-    executes an command line ip tables command
+    executes an command line ip tables command to allow connection to the webserver
     """
     
     # TODO: generalize this for multiple comnads
