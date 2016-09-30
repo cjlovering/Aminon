de5914578c387bbb29350ca31bb336feb4786b38
jograpes <jabubes@yandex.com>
2016-09-29 22:40:57 -0400

    Update dig_avgz.sh

--- dig_avgz.sh
+++ dig_avgz.sh
@@ -9,9 +9,10 @@ done
 # sh qr.sh | grep Query >> site.cut
 # subl site.cut #eliminate non numerics
 # awk '{ sum += $1 } END { print sum }' site.cut
-
+# sort -t= -nr -k3 site.cut| tail -1 # yeilds the min
+# sort -t= -nr -k3 site.cut| head -1 # yeilds the max
 
 ##ANS##
-# goo average: 3182/150 = 21.213333333
-# pi average: 3026/150 = 20.173333333
-# 8s average: 2793/150 = 18.62
+# goo average: 3182/150 = 21.213333333 min: 10 max: 41
+# pi average: 3026/150 = 20.173333333 min: 12 max: 97
+# 8s average: 2793/150 = 18.62 		    min: 14 max: 99
