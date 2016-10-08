375f444a2ee3c0917ff3538697f7001798f32565
jograpes <jabubes@yandex.com>
2016-10-07 20:11:55 -0400

    Soul Cleansing

--- dns_nat_arbiter.py
+++ dns_nat_arbiter.py
@@ -14,50 +14,6 @@ ip_list = [] # global list of randomly generated IPs
 ttl = 300
 ttl_map = {}
 
-
-def initial_configure():
-    """
-    generates list of public ips and stores it in store
-    writes the pre-routing rule to send the honeypot
-    """
-    # creates a dict of ip addresses
-
-    # this rule is needed to route dns resp to netfilter q
-    # > sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 1
-    # We don't need sudo as the script will run as sudo python script.py
-    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-p", "udp", "--dport", "53", "-j", "NFQUEUE" "--queue-num", "1"], stdout=subprocess.PIPE)
-    
-    
-    
-    # write the pre-routing rule to route everything to the honeypot unless otherwise specified
-    """
-    iptables -t mangle -A PREROUTING -i eth0 -j TTL --ttl-set 64
-    
-    iptables -A FORWARD -d 2.2.2.2 -i eth0 -p tcp -m tcp --dport 1000:65500 -j ACCEPT #forward tcp port range
-    iptables -A FORWARD -d 2.2.2.2 -i eth0 -p udp -m udp --dport 1000:65500 -j ACCEPT #forward udp port range
-    iptables -t nat -A PREROUTING -d 1.1.1.1 -p tcp -m tcp --dport 1000:65500 -j DNAT --to-destination 2.2.2.2  #tcp port range
-    iptables -t nat -A PREROUTING -d 1.1.1.1 -p udp -m udp --dport 1000:65500 -j DNAT --to-destination 2.2.2.2  #udp port range
-    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    
-    ... general iptables ...
-    -t nat/filter/mangle
-    -A append to list -I insert
-    -d destination adress
-    -i input interface (where pkt recv)
-    -o output interface (where pkt sent)
-    route from IP:port to another IP:port
-    iptables -t nat -A PREROUTING --src $SRC_IP_MASK --dst $DST_IP -p tcp --dport $portNumber -j REDIRECT --to-ports $rediectPort
-    will attempt to update tomo
-    """
-    sp = subprocess.Popen(["iptables", "-t", "mangle", "-A", "PREROUTING", "-i", interface, "-j", "TTL", "--ttl-set", "64"], stdout=subprocess.PIPE)
-    
-    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", interface, "-p", "tcp", "-m", "tcp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-A", "FORWARD", "-d", honeypot_addr, "-i", interface, "-p", "udp", "-m", "udp", "--dport", "1000:65500" , "-j", "ACCEPT"], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-d", client_addr, "-p", "tcp", "-m", "tcp", "--dport", "1000:65500" , "-j", "DNAT", "--to-destination", honeypot_addr], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "PREROUTING", "-d", client_addr, "-p", "udp", "-m", "udp", "--dport", "1000:65500" , "-j", "DNAT", "--to-destination", honeypot_addr], stdout=subprocess.PIPE)
-    sp = subprocess.Popen(["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", interface, "-j", "MASQUERADE"], stdout=subprocess.PIPE)
-    output , err = sp.communicate()
-    print output
     
 def gen_rand_ip():
     first_blk = random.randint(0, 255)
