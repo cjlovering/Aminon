def38594a9a154eac3bc0663b67eff5684d622d0
jograpes <jabubes@yandex.com>
2016-10-07 23:36:36 -0400

    Create honeyVB2.sh

---
+++ honeyVB2.sh
@@ -0,0 +1,7 @@
+
+# DISCLAIMER COMMANDS ARE DIFFERENT FROM NAT
+#Run w/sudo 
+
+# May potentially needs a netmask, this may work
+#ip address add 10.4.6.4/32 netmask 255.255.255.0 dev eth0
+ip address add 10.4.6.4/32 dev eth0
