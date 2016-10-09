88322a0aa5a5fb6bc33cdd5ef598dea81cbde473
jograpes <jabubes@yandex.com>
2016-10-09 17:44:16 -0400

    Update natVB1.sh

--- configuration/client-attacker/natVB1.sh
+++ configuration/client-attacker/natVB1.sh
@@ -4,4 +4,4 @@
 
 # May potentially needs a netmask, this may work
 #ip address add 10.4.6.128/25 netmask 255.255.255.0 dev eth0
-ip address add 10.4.6.128/25 dev eth0
+ip address add 10.4.6.128/25 via 10.4.6.1
